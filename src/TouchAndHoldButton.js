a1d120975d07602ecf69f29e3e971278e7ab02b3
shaack <shaack@gmail.com>
2023-10-09 17:48:55 +0200

    added `confirmed` event. Overall not compatible with v1.

--- src/TouchAndHoldButton.js
+++ src/TouchAndHoldButton.js
@@ -3,74 +3,78 @@
  * Repository: https://github.com/shaack/cm-chessboard
  * License: MIT, see file 'LICENSE'
  */
+export class TouchAndHoldButton {
 
-export function TouchAndHoldButton(buttonElement, props = {}) {
-
-    this.props = {
-        holdDuration: 1000,
-        preventDefault: true,
-        fillColor: "rgba(0,0,0,0.1)"
-    }
-    Object.assign(this.props, props)
-
-    let holdTimeout
-
-    buttonElement.style.position = "relative"
-    this.fillElement = document.createElement('span')
-    this.fillElement.classList.add('fill')
-    this.fillElement.style.position = "absolute"
-    this.fillElement.style.top = "0"
-    this.fillElement.style.left = "0"
-    this.fillElement.style.height = "100%"
-    this.fillElement.style.width = "0" // Initial state: no fill
-    this.fillElement.style.backgroundColor = this.props.fillColor // This is the color that will fill the button
-    this.fillElement.style.zIndex = "0"
-    this.fillElement.style.transition = `width ${this.props.holdDuration / 1000}s linear`
-    buttonElement.append(this.fillElement)
-
-    if (this.props.preventDefault) {
-        buttonElement.addEventListener("click", (event) => {
-            event.preventDefault()
-        })
-    }
-
-    const startHold = () => {
-        addPointerUpListener(buttonElement, stopHold)
-        this.fillElement.style.width = "100%"  // This triggers the fill effect
-        holdTimeout = setTimeout(() => {
-            buttonElement.dispatchEvent(new Event('touchAndHold'))
-            stopHold()
-        }, this.props.holdDuration)
-    }
-
-    const stopHold = () => {
-        buttonElement.removeTouchAndHoldPointerUpListener()
-        clearTimeout(holdTimeout)
-        this.fillElement.style.width = "0"
-        this.fillElement.style.transition = "none" // "width 0.05s ease-in-out"  // This resets the fill effect
-
-        setTimeout(() => {
-            this.fillElement.style.transition = `width ${this.props.holdDuration / 1000}s linear`
-        }, 50)
-    }
-
-    function addPointerDownListener(element, callback) {
-        element.addEventListener('pointerdown', callback)
-        element.addEventListener('touchstart', callback)
-        element.removeTouchAndHoldPointerDownListener = () => {
-            element.removeEventListener('pointerdown', callback)
-            element.removeEventListener('touchstart', callback)
+    constructor(buttonElement, props = {}) {
+        this.props = {
+            holdDuration: 1000,
+            fillColor: "rgba(0,0,0,0.1)",
+            confirmedShadow: "0 0 0 5px rgba(0,100,0,0.5)"
         }
-    }
-
-    function addPointerUpListener(element, callback) {
-        window.addEventListener('pointerup', callback)
-        window.addEventListener('touchend', callback)
-        element.removeTouchAndHoldPointerUpListener = () => {
-            window.removeEventListener('pointerup', callback)
-            window.removeEventListener('touchend', callback)
+        Object.assign(this.props, props)
+        this.state = {
+            confirmed: false,
+            buttonOriginalBoxShadow: buttonElement.style.boxShadow
         }
-    }
+        let holdTimeout
 
-    addPointerDownListener(buttonElement, startHold)
+        buttonElement.style.position = "relative"
+        this.fillElement = document.createElement('span')
+        this.fillElement.classList.add('fill')
+        this.fillElement.style.position = "absolute"
+        this.fillElement.style.top = "0"
+        this.fillElement.style.left = "0"
+        this.fillElement.style.height = "100%"
+        this.fillElement.style.width = "0" // Initial state: no fill
+        this.fillElement.style.backgroundColor = this.props.fillColor // This is the color that will fill the button
+        this.fillElement.style.zIndex = "0"
+        this.fillElement.style.transition = `width ${this.props.holdDuration / 1000}s linear`
+        buttonElement.append(this.fillElement)
+
+        const startHold = () => {
+            this.state.confirmed = false
+            addPointerUpListener(buttonElement, stopHold)
+            this.fillElement.style.width = "100%"  // This triggers the fill effect
+            holdTimeout = setTimeout(() => {
+                this.state.confirmed = true
+                buttonElement.style.boxShadow = this.props.confirmedShadow
+                buttonElement.dispatchEvent(new Event('confirmed'))
+            }, this.props.holdDuration)
+        }
+
+        const stopHold = () => {
+            buttonElement.removeTouchAndHoldPointerUpListener()
+            clearTimeout(holdTimeout)
+            this.fillElement.style.width = "0"
+            this.fillElement.style.transition = "none" // "width 0.05s ease-in-out"  // This resets the fill effect
+            buttonElement.style.boxShadow = this.state.buttonOriginalBoxShadow
+
+            setTimeout(() => {
+                this.fillElement.style.transition = `width ${this.props.holdDuration / 1000}s linear`
+            }, 50)
+        }
+
+        function addPointerDownListener(element, callback) {
+            element.addEventListener('pointerdown', callback)
+            element.addEventListener('touchstart', callback)
+            element.removeTouchAndHoldPointerDownListener = () => {
+                element.removeEventListener('pointerdown', callback)
+                element.removeEventListener('touchstart', callback)
+            }
+        }
+
+        function addPointerUpListener(element, callback) {
+            window.addEventListener('pointerup', callback)
+            window.addEventListener('touchend', callback)
+            element.removeTouchAndHoldPointerUpListener = () => {
+                window.removeEventListener('pointerup', callback)
+                window.removeEventListener('touchend', callback)
+            }
+        }
+
+        addPointerDownListener(buttonElement, startHold)
+    }
+    confirmed() {
+        return this.state.confirmed
+    }
 }
